23d2d31360d4515ef5c3b8dafb23a151f25de0bc
pupupopo88 <39chenas@gmail.com>
2015-09-05 14:19:18 +0900

    reword engawa

--- lib/engawa_eater/cli.rb
+++ lib/engawa_eater/cli.rb
@@ -5,20 +5,20 @@ module EngawaEater
   class CLI < Thor
     desc "start", "play the eat a lot engawa game."
     def start
-      puts "eat only '炙りエンガワ'（e:eat, p:pass）"
-      sushis = ["炙りエンガワ", "エンガワ", "炙りサーモン"]
+      puts "eat only '炙りえんがわ'（e:eat, p:pass）"
+      sushis = ["炙りえんがわ", "えんがわ", "炙りサーモン"]
       score = 0
       10.times do
         gave_sushi = sushis.sample
         puts gave_sushi
         action = STDIN.gets.chomp!
-        if gave_sushi == "炙りエンガワ"
+        if gave_sushi == "炙りえんがわ"
           score += 1 if action == "e"
         else
           score += 1 if action == "p"
         end
       end
-      puts "炙りエンガワ #{score * 10}%！"
+      puts "炙りえんがわ #{score * 10}%！"
     end
   end
 end
