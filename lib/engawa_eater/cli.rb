995112dcdd70c47656d77a3d2182ac54e4f23bd1
Ohtsuka Takahiro <t.o.0526@gmail.com>
2015-09-06 20:44:02 +0900

    many_start_mesasge method

--- lib/engawa_eater/cli.rb
+++ lib/engawa_eater/cli.rb
@@ -18,58 +18,11 @@ module EngawaEater
       puts "炙りえんがわ #{score.nil? ? 0 : score * 10}%！"
     end
 
-    private
-
-    def only_start_mesasge
-      puts <<-EOS
-----------------------------------------
-eat only '炙りえんがわ'（e:eat, p:pass）
-----------------------------------------
-Start: Press Enter
-      EOS
-    end
-
-    def questions(all_sushi)
-      Array.new(10).map { all_sushi.sample }
-    end
-
-    def run(all_sushi)
-      questions(all_sushi).map do |sushi|
-        puts sushi.name
-        aburi_engawa_check(sushi)
-      end.compact.inject(:+)
-    end
-
-    def aburi_engawa_check(sushi)
-      timeout(1.2) do
-        case STDIN.gets.chomp!
-        when 'e' then sushi.score if sushi.name == '炙りえんがわ'
-        when 'p' then sushi.score unless sushi.name == '炙りえんがわ'
-        end
-      end
-    rescue Timeout::Error
-      # 何もしない
-    end
-  end
-  CLI.start(ARGV)
-end
-
-=begin
     desc "many", "Game of continue to eat 'えんがわ' and '炙りえんがわ'."
     def many
       sec = 30
       max_stomach_capacity = 20
-      puts <<-EOS
---------------------------------------------------
-continue to eat '炙りえんがわ' and 'えんがわ'（e:eat, p:pass）
-Time limit:#{sec}sec, Max stomach capacity:#{max_stomach_capacity}
-炙りえんがわ（score:+3, stomach status:+4）
-えんがわ（score:+1, stomach status:+2）
-芽ねぎ（stomach status:-4）
-Oters（stomach status:+2）
---------------------------------------------------
-Start: Press Enter
-      EOS
+      many_start_mesasge(sec, max_stomach_capacity)
       STDIN.gets
       sushis = ["炙りえんがわ", "えんがわ", "芽ねぎ", "炙りほたて", "マグロ", "サーモン"]
       stomach_status = 0
@@ -103,4 +56,53 @@ Start: Press Enter
         puts "Time's UP!（SCORE:#{score}, STOMACH:#{stomach_status}）"
       end
     end
-=end
+
+    private
+
+    def only_start_mesasge
+      puts <<-EOS
+----------------------------------------
+eat only '炙りえんがわ'（e:eat, p:pass）
+----------------------------------------
+Start: Press Enter
+      EOS
+    end
+
+    def many_start_mesasge(sec, max_stomach_capacity)
+      puts <<-EOS
+--------------------------------------------------
+continue to eat '炙りえんがわ' and 'えんがわ'（e:eat, p:pass）
+Time limit:#{sec}sec, Max stomach capacity:#{max_stomach_capacity}
+炙りえんがわ（score:+3, stomach status:+4）
+えんがわ（score:+1, stomach status:+2）
+芽ねぎ（stomach status:-4）
+Other（stomach status:+2）
+--------------------------------------------------
+Start: Press Enter
+      EOS
+    end
+
+    def questions(all_sushi)
+      Array.new(10).map { all_sushi.sample }
+    end
+
+    def run(all_sushi)
+      questions(all_sushi).map do |sushi|
+        puts sushi.name
+        aburi_engawa_check(sushi)
+      end.compact.inject(:+)
+    end
+
+    def aburi_engawa_check(sushi)
+      timeout(1.2) do
+        case STDIN.gets.chomp!
+        when 'e' then sushi.score if sushi.name == '炙りえんがわ'
+        when 'p' then sushi.score unless sushi.name == '炙りえんがわ'
+        end
+      end
+    rescue Timeout::Error
+      # 何もしない
+    end
+  end
+  CLI.start(ARGV)
+end
