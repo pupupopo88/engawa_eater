f5c17a0d5530a65a72e7b7f1d7dbd772fa06c1f2
pupupopo88 <39chenas@gmail.com>
2015-09-06 10:21:23 +0900

    delete test_it_does_something_useful

--- test/engawa_eater_test.rb
+++ test/engawa_eater_test.rb
@@ -4,8 +4,4 @@ class EngawaEaterTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::EngawaEater::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
